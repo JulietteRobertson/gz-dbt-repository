4d108e91f98307c552efd64db77b9b74b64edadc
Juliette Robertson <julietterobertsonroux@gmail.com>
2024-10-29 14:15:27 +0000

    raaaaaa

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,7 @@
+SELECT 
+SUM(revenue) AS revenue,
+SUM(quantity) AS quantity, 
+SUM(purchase_cost) AS purchase_cost,
+SUM(margin) AS tot_margin
+FROM {{ref("int_sales_margin")}}
+GROUP BY date_date, orders_id
