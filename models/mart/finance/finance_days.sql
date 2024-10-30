c4e703dd61d8db9be725985673bda3bd72fb9d74
Juliette Robertson <julietterobertsonroux@gmail.com>
2024-10-30 13:48:30 +0000

    c'est pas versailles ici

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -1,14 +1,24 @@
-SELECT
-date_date,
-COUNT(orders_id) AS nb_transactions, 
-SUM(revenue) AS tot_revenue,
-AVG(revenue) AS average_basket,
-SUM(tot_margin) AS margin, 
-SUM(tot_margin-logcost) AS operational_margin, 
-SUM(purchase_cost) AS tot_purchase_cost,
-SUM(shipping_fee) AS tot_shipping_fee,
-SUM(quantity) AS tot_quantity,
-SUM(logcost) AS tot_logcost
-FROM {{ref("int_orders_operational")}}
-GROUP BY date_date
-ORDER BY date_date DESC
+WITH revenue_subquery AS (
+        SELECT date_date
+        ,COUNT(DISTINCT orders_id) AS nb_transactions
+        ,ROUND(SUM(revenue),2) AS revenue
+        ,ROUND(SUM(revenue)/COUNT(DISTINCT orders_id)) AS average_basket
+        FROM {{ref("int_orders_margin")}}
+        GROUP BY date_date
+        ORDER BY date_date DESC
+    )
+,
+    margin_subquery AS (
+        SELECT date_date
+        ,ROUND(SUM(tot_margin),2) AS margin
+        ,ROUND (SUM(Operational_margin),2) AS operationnal_margin
+        ,SUM(quantity) AS quantity
+        FROM {{ref("int_orders_operational")}}
+        GROUP BY date_date
+        ORDER BY date_date
+    )
+    SELECT *
+    FROM revenue_subquery
+    INNER JOIN margin_subquery
+    USING (date_date)
+    ORDER BY date_date DESC
