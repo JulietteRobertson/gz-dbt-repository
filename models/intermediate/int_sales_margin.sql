a831ba8e23475fec35bce0296e65d20f3bb5d9a5
Juliette Robertson <julietterobertsonroux@gmail.com>
2024-10-30 15:23:35 +0000

    c'est la finnnn

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,5 +5,6 @@ USING (products_id))
 
 SELECT *,
 quantity*purchase_price AS purchase_cost,
-ROUND(revenue-(quantity*purchase_price),2) AS margin
+ROUND(revenue-(quantity*purchase_price),2) AS margin,
+{{ margin_percent('revenue', 'quantity*purchase_price') }} AS margin_percent
 FROM sub1
--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -13,6 +13,10 @@ WITH revenue_subquery AS (
         ,ROUND(SUM(tot_margin),2) AS margin
         ,ROUND (SUM(Operational_margin),2) AS operationnal_margin
         ,SUM(quantity) AS quantity
+        ,SUM(purchase_cost) AS purchase_cost
+        ,SUM (shipping_fee) AS shipping_fee
+        ,SUM(logcost) AS logcost
+        ,SUM(ship_cost) As shipcost
         FROM {{ref("int_orders_operational")}}
         GROUP BY date_date
         ORDER BY date_date
